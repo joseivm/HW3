eaa1be01b42587e9febd84ebfcf369c1c3cd51ef
Jeffrey Hu <hujh@mit.edu>
2016-11-07 22:41:32 -0500

    cleaned up NN

--- hw3_resources/NN.py
+++ hw3_resources/NN.py
@@ -10,28 +10,34 @@ class Neural_Network(object):
 
 
 	def __init__(self, layer_sizes):
-		self.num_layers = len(layer_sizes)
-		self.layer_sizes = layer_sizes
-		self.biases = [np.random.rand(size,1) for size in layer_sizes[1:]]
-		self.weights = [np.random.rand(x,y) for x,y in zip(layer_sizes[:-1],layer_sizes[1:0])]
-
+		self.layers = []
+		input_size = layer_sizes[0]
+		for layer_size in layer_sizes[1:]:
+			self.layers.append(NNLayer(input_size, layer_size))
+			input_size = layer_size
 
 	# x should be an (n,1) array
 	def feedforward(self,x):
-		inputt = x
-		for W,b in zip(self.weights,self.biases)[:-1]:
-			inputt = ReLU(np.dot(W.T,inputt)+b)
+		output = x
+		for layer in self.layers:
+			output = layer.feedforward(self, output)
+		return softmax(output)
 
-		W = self.weights[-1]
-		b = self.biases[-1]
-		output softmax(np.dot(W.T,inputt)+b)
 
-		return output
+	def softmax(x):
+		e_x = np.exp(x-np.max(x))
+		return e_x/e_x.sum()
+
+class NNLayer(object):
+
+	def __init__(self, input_size, num_of_units):
+		self.weights = np.random.rand(input_size, num_of_units)
+		self.bias = np.random.rand(size,1)
+
+	def feedforward(self, x):
+		return ReLU(np.dot(self.weights.T, x) + self.bias)
 
 	def ReLU(x):
 		zero = np.zeros([len(x),1])
 		return np.maximum(zero,x)
-
-	def softmax(x):
-		return np.exp(x)/np.sum(np.exp(x))
 
